5f3ef102d8fba5cbb91a5ca6a88dbb48477cce30
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-05-21 04:38:30 -0700

    Touched up background and font color. Complete

--- af6/src/components/Signup.js
+++ af6/src/components/Signup.js
@@ -9,16 +9,8 @@ const initialFormValues = {
   role: "",
 };
 
-const initialFormErrors = {
-  firstname: "",
-  lastname: "",
-  email: "",
-  role: "",
-};
-
 const SignUp = () => {
   const [formValues, setFormValues] = useState(initialFormValues);
-  const [formErrors, setFormErrors] = useState(initialFormErrors);
 
   const handleChange = (e) => {
     setFormValues({ ...formValues, [e.target.name]: e.target.value });
@@ -35,16 +27,7 @@ const SignUp = () => {
       })
       .catch((err) => {
         console.log({ "err:": err.response.data });
-        handleErrors();
       });
-  };
-
-  const handleErrors = () => {
-    if (
-      formValues.username !== "eve.holt@reqres.in" ||
-      formValues.password !== "pistol"
-    )
-      setFormErrors("Username or Password not valid");
   };
 
   return (
@@ -102,17 +85,16 @@ const SignUp = () => {
       <div>
         <label id="role-dropdown">
           <select name="role" value={formValues.role} onChange={handleChange}>
-            <option>--Select a Role--</option>
             <option value="client">Client</option>
             <option value="instructor">Instructor</option>
           </select>
         </label>
         <label>
           Auth Code:
-          <input type="text" name="authcode" placeholder="Instructor Only" />
+          <input type="text" name="authcode" placeholder="Instructor Only..." />
         </label>
       </div>
-
+      <br />
       <button type="submit" className="btn btn-dark btn-block">
         Register
       </button>
@@ -124,90 +106,3 @@ const SignUp = () => {
 };
 
 export default SignUp;
-
-// import React, { useState } from "react";
-// import axiosWithAuth from "../helpers/axiosWithAuth";
-
-// const initialFormValues = {
-//   username: "",
-//   password: "",
-//   email: "",
-//   role: ""
-// };
-
-// const Signup = () => {
-//   const [formValues, setFormValues] = useState(initialFormValues);
-//   const [error, setError] = useState("");
-
-//   const handleChange = (e) => {
-//     setFormValues({ ...formValues, [e.target.name]: e.target.value });
-//   };
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     axiosWithAuth()
-//       .post("/api/register", formValues)
-//       .then((res) => {
-//         console.log("res:", res);
-//         //res.data.payload gives us token
-//         localStorage.setItem("token", res.data.payload);
-//         //   props.history.push('/protected/bubbles')
-//       })
-//       .catch((err) => {
-//         console.log({ "err:": err.response.data });
-//         handleErrors();
-//       });
-//   };
-
-//   const handleErrors = () => {
-//     if (
-//       formValues.username !== "eve.holt@reqres.in" ||
-//       formValues.password !== "pistol"
-//     )
-//       setError("Username or Password not valid");
-//   };
-//   return (
-//     <div>
-//       <h1>Sign up for Anywhere Fitness!</h1>
-//       <div className="login-form"></div>
-//       <form onSubmit={handleSubmit}>
-//         <label htmlFor="email">Email:</label>
-//         <input
-//           type="text"
-//           name="email"
-//           value={formValues.email}
-//           onChange={handleChange}
-//           id="email"
-//           placeholder="Email..."
-//           />
-//         <label htmlFor="username">Username:</label>
-//         <input
-//           type="text"
-//           name="username"
-//           value={formValues.username}
-//           onChange={handleChange}
-//           id="username"
-//           placeholder="Username..."
-//         />
-//         <label htmlFor="password">Password:</label>
-//         <input
-//           type="password"
-//           name="password"
-//           value={formValues.password}
-//           onChange={handleChange}
-//           id="password"
-//           placeholder="Password..."
-//         />
-//         <label>
-//           <select id="role-dropdown" name="role" value={formValues.role} onChange={handleChange}>
-//             <option>--Select a role--</option>
-//             <option value="client">Client</option>
-//             <option value="instructor">Instructor</option>
-//           </select>
-//         </label>
-//         <button>Join!</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Signup;
